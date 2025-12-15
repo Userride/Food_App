684d4532b102c1e5a49b98b813ef5740e1288371
prince kumar <125152568+Userride@users.noreply.github.com>
2025-12-15 13:40:29 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,26 +1,36 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const{Schema}=mongoose;
-const UserSchema=new Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    location:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-   date:{
-        type:Date,
-        default:Date.now 
-    }
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    default: ''
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    default: '' // empty for Google users
+  },
+  googleId: {
+    type: String,
+    default: ''
+  },
+  avatar: {
+    type: String,
+    default: ''
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
-module.exports=mongoose.model('user',UserSchema)
+
+module.exports = mongoose.model('user', UserSchema);
